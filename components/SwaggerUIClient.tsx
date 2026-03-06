c014fc0ea0be1cc4808646a06acee4a44601b238
Harshan Aiyappa <100086678+Kimosabey@users.noreply.github.com>
2026-03-06 10:19:58 +0530

    Update SwaggerUIClient.tsx

--- components/SwaggerUIClient.tsx
+++ components/SwaggerUIClient.tsx
@@ -6,5 +6,5 @@ import 'swagger-ui-react/swagger-ui.css'
 import '@/styles/swagger-overrides.css'
 
 export function SwaggerUIClient() {
-  return <SwaggerUI url="/api/docs" />
+  return <SwaggerUI url="/swagger.json" />
 }
