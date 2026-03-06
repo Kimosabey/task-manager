94f16a536664fc97105ae13ba464995522c50445
Harshan Aiyappa <100086678+Kimosabey@users.noreply.github.com>
2026-03-06 10:23:44 +0530

    Add experimental outputFileTracingExcludes to config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': [
+        '.next/cache/**',
+        'node_modules/@swc/**',
+      ],
+    },
+  },
+};
 
 export default nextConfig;
